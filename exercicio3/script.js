80a319beab1e1a9278aef569fbef955a3727ef68
francinaldapmfaria <portela854@gmail.com>
2022-09-06 16:10:33 -0300

    Exercícios realizado 1-2-3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,21 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui sua nacionalidade")
+
+if(nacionalidade === "Brasileira"){
+    console.log("Brasileira")
+
+}else if(nacionalidade === "Argentina"){
+    console.log("Argentino")
+	
+}else if(nacionalidade === "Uruguai"){
+    console.log("Uruguai")
+
+} else if(nacionalidade === "Chilena"){
+    console.log("Chilena")
+
+} else if(nacionalidade === "Colombiana"){
+    console.log("Colombiana")
+
+} else{
+    console.log("Nacionalidade não corresponde a nenhum dos valores")
+}
+
